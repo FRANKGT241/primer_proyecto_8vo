be7fa53c608788faa12194c9e52af900f366022a
Hanz Maverick <maverick_ruiz@mesoamericana.edu.gt>
2024-08-10 11:05:26 -0600

    add "auth" to non-perishable products route

--- src/routes/nonPerishableProductsRoutes.js
+++ src/routes/nonPerishableProductsRoutes.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const nonPerishableProductController = require('../controllers/nonPerishableProductsController');
+const authController = require('../controllers/authController');
 
 // Rutas CRUD
 router.post('/nonPerishableProducts', nonPerishableProductController.createProduct);
+
 router.get('/nonPerishableProducts', nonPerishableProductController.getAllProducts);
+
 router.get('/nonPerishableProducts/:id', nonPerishableProductController.getProductById);
+
 router.put('/nonPerishableProducts/:id', nonPerishableProductController.updateProduct);
+
 router.delete('/nonPerishableProducts/:id', nonPerishableProductController.deleteProduct);
 
-module.exports = router;
+module.exports = (app) => {
+    // Todas las rutas bajo la autenticación `/web/api`, protegido por autenticación
+    app.use('/web/api', authController.authenticateToken, router);
+};
