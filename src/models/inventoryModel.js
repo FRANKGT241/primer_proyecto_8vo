959cf4b2f687ae659b0bdd99d17e0770e758a8b5
FRANKGT241 <mau910462@gmail.com>
2024-08-12 19:25:34 -0600

    Fix models and testing sales

--- src/models/inventoryModel.js
+++ src/models/inventoryModel.js
@@ -1,36 +1,36 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../database');
 
-const SalesDetail = sequelize.define('SalesDetail', {
-  detail_id: {
+const Inventory = sequelize.define('Inventory', {
+  inventory_id: {
     type: DataTypes.INTEGER,
     autoIncrement: true,
     primaryKey: true,
   },
-  inventory_id: {
+  product_id: {
     type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  product_type: {
+    type: DataTypes.ENUM('Perishable', 'Non Perishable'),
     allowNull: false,
   },
   quantity: {
     type: DataTypes.INTEGER,
     allowNull: false,
   },
-  unit_price: {
-    type: DataTypes.DECIMAL(10, 2),
+  last_updated: {
+    type: DataTypes.DATE,
     allowNull: false,
   },
   is_active: {
     type: DataTypes.TINYINT(1),
     allowNull: false,
     defaultValue: 1,
-  },
-  sale_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
   }
 }, {
-  tableName: 'sales_details',
+  tableName: 'inventory',
   timestamps: false,
 });
 
-module.exports = SalesDetail;
+module.exports = Inventory;
--- src/models/salesModel.js
+++ src/models/salesModel.js
@@ -1,46 +1,32 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../database');
-const SalesDetail = require('./salesDetailsModel');
 
 const Sale = sequelize.define('Sale', {
-    sale_id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-    },
-    date: {
-        type: DataTypes.DATE,
-        allowNull: false,
-    },
-    total: {
-        type: DataTypes.DECIMAL(10, 2),
-        allowNull: false,
-    },
-    user_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'users',
-            key: 'user_id'
-        }
-    },
-    customer_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'customers',
-            key: 'customer_id' //Customer no se incluye en el diagrama de base de datos
-        }
-    },
-    is_active: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        defaultValue: true,
-    },
+  sale_id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  total: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  is_active: {
+    type: DataTypes.TINYINT(1),
+    allowNull: false,
+    defaultValue: 1,
+  }
 }, {
-    tableName: 'sales',
-    timestamps: false,
+  tableName: 'sales',
+  timestamps: false,
 });
 
-Sale.hasMany(SalesDetail, { foreignKey: 'sale_id', as: 'details' });
 module.exports = Sale;
